8a91739cb58d3529f4eacc33dadfeeda0d575957
Elizabeth Villalejos <nekobanpaia@gmail.com>
2021-01-07 10:41:56 -0700

    EventCard component created and loaded to homepage

---
+++ src/components/EventCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="event-card">
+    <!-- Display data -->
+    <span>@ {{ event.time  }} on {{event.date}} </span>
+    <h4>{{ event.title }}</h4>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    events: Object
+  },
+  data() {
+    return {
+      event: {
+        id: 1232452,
+        category: 'animal welfare',
+        title: 'Chi adoption day',
+        description: 'Get a chihuahua into your home',
+        location: 'Woof Park',
+        date: 'January 10, 2021',
+        time: '5:00',
+        petsAllowed: true,
+        organizer: 'Dulce'
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+        id: 1232452,
+        category: 'animal welfare',
+        title: 'Chi adoption day',
+        description: 'Get a chihuahua into your home',
+        location: 'Woof Park',
+        date: 'January 10, 2021',
+        time: '5:00',
+        petsAllowed: true,
+        organizer: 'Dulce'
+      },
+      {
+        id: 123245332,
+        category: 'animal welfare',
+        title: 'Chi adoption day',
+        description: 'Get a chihuahua into your home',
+        location: 'Woof Park',
+        date: 'January 10, 2021',
+        time: '5:00',
+        petsAllowed: true,
+        organizer: 'Dulce'
+      },
+      {
+        id: 123222452,
+        category: 'animal welfare',
+        title: 'Chi adoption day',
+        description: 'Get a chihuahua into your home',
+        location: 'Woof Park',
+        date: 'January 10, 2021',
+        time: '5:00',
+        petsAllowed: true,
+        organizer: 'Dulce'
+      }
+      ]
+    }
   }
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
